bd4b8d97ef5a82870c03bb7e0599659361518d1d
butzlaff <emiliobutz@gmail.com>
2023-11-15 19:39:44 -0300

    Starting the project

---
+++ script.sh
@@ -0,0 +1,35 @@
+npm i @types/bcryptjs -D
+npm i @types/cors -D
+npm i @types/express -D
+npm i @types/jsonwebtoken -D
+npm i @types/node -D
+npm i @typescript-eslint/eslint-plugin -D
+npm i @typescript-eslint/parser -D
+npm i eslint -D
+npm i eslint-config-airbnb-base -D
+npm i eslint-config-airbnb-typescript -D
+npm i eslint-plugin-import -D
+npm i eslint-plugin-sonarjs -D
+npm i nodemon -D
+npm i ts-node -D
+npm i ts-node-dev -D
+npm i typescript -D
+npm install mongoose
+npm install mongodb
+npm i bcryptjs
+npm i cors
+npm i express
+npm i express-async-errors
+npm i jest
+npm i jsonwebtoken
+mkdir src
+cd src
+mkdir controllers
+mkdir services
+mkdir middlewares
+mkdir models
+mkdir routes
+mkdir utils
+touch app.ts
+touch index.ts
+touch server.ts
